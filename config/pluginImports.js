a1da12efef233dee483d1d7c3785b16c19e74ea3
transse04731 <triracle@gmail.com>
2020-02-24 12:07:09 +0700

    First commit

---
+++ config/pluginImports.js
@@ -0,0 +1,19 @@
+import cms from 'cms';
+
+
+//repair path here
+// const req = require.context('../../plugins/attendance-plugin/components', true, /.*\.vue$/);
+// req.keys().map(file => {
+//   const component = req(file).default;
+//   const compName = file.split('/').pop().split('.vue')[0];
+//   cms.registerComponent(compName, {}, component);
+// });
+
+const cloudSignageReq = require.context('../plugins/cloud-signage-plugin/components', true, /.*\.vue$/);
+cloudSignageReq.keys().map(file => {
+  const component = cloudSignageReq(file).default;
+  const compName = file.split('/').pop().split('.vue')[0];
+  cms.registerComponent(compName, {}, component);
+});
+
+// cms.registerComponent('TestImport', {}, TestImport);
