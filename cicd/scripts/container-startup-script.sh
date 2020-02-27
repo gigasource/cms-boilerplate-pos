a78b13d8e122d89e5f7c01b582c330409fddf22b
danglam105 <danglam105@gmail.com>
2020-02-27 15:00:04 +0700

    Move CICD files to /cicd folder

---
+++ cicd/scripts/container-startup-script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
+GITBOT_ACCESS_TOKEN=$(cat ./gitbot-access-token)
+rm ./gitbot-access-token
+git config --global url."https://${GITBOT_ACCESS_TOKEN}:@github.com/".insteadOf "https://github.com/"
+git config --global user.email "gitbot@gigasource.io"
+git config --global user.name "Gigasource Gitbot"
+node backend/use/index.js --config=./config/config.json
