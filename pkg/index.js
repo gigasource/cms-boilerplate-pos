4bd80080cf8f0a5828db3f97b9c08dffe6c98ebe
transse04731 <triracle@gmail.com>
2020-03-04 18:55:12 +0700

    add file for Jenkins

--- pkg/index.js
+++ pkg/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-global.phantomPath = path.resolve(process.argv[0], '../phantomjs');
+global.phantomPath = path.resolve(process.argv[0], '../phantomjs.exe');
 process.env.CONFIG_PATH = path.resolve(__dirname, '../config/config.json');
 console.log(process.env.CONFIG_PATH);
 require('../cms/backend/use');
