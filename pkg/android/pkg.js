e63a4b0b8e2ce04986931e84a4bd26b4bf00a9a7
transse04731 <triracle@gmail.com>
2020-03-05 17:09:14 +0700

    add build for android

--- pkg/android/pkg.js
+++ pkg/android/pkg.js
@@ -1,4 +1,4 @@
 require('giga-pkg-patches');
-const { exec } = require('pkg/android/pkg');
-const argv = ['--config', 'pkg.config.js', '--targets', 'node10-macos-x64', '--output', 'out/app', 'index.js', '--no-bytecode', '--public', '--public-packages', '*'];
+const { exec } = require('pkg');
+const argv = ['--config', 'pkg.config.js', '--targets', 'node10-linux-x64', '--output', '../android/pos-android/app/src/main/assets/appAssets/app', 'android/index.js', '--no-bytecode', '--public', '--public-packages', '*'];
 exec(argv);
